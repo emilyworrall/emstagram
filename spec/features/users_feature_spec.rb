2011de4cf5a80936460675f639915df124539113
Emily Worrall <worrall.emily@gmail.com>
2015-11-19 17:53:50 +0000

    Users feature spec complete

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 feature 'Users' do
+  include Helpers
+
   context 'not signed in and on the homepage' do
     it "should see a 'Login' link and a 'Sign Up' link" do
       visit '/'
@@ -10,6 +12,22 @@ feature 'Users' do
   end
 
   context 'signed in and on the homepage' do
-  
+    let!(:user){User.create(email: 'emily@example.com',
+                            password: 'Pa55w0rd')}
+
+    before do
+      login(user)
+    end
+
+    it 'should see "Sign Out" link' do
+      visit '/'
+      expect(page).to have_link('Sign Out')
+    end
+
+    it 'should not see a "Login" link and a "Sign Up" link' do
+      visit '/'
+      expect(page).not_to have_link('Login')
+      expect(page).not_to have_link('Sign Up')
+    end
   end
 end
